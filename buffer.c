ee50f652416545832be004b5f9c4ffd78ecca6eb
Seong-Kook Shin (@tempus) <cinsky@gmail.com>
2014-04-02 23:13:05 +0900

    add _buffer_ module

---
+++ buffer.c
@@ -0,0 +1,550 @@
+#define _GNU_SOURCE
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>             /* for memmem(3) */
+#include <stdlib.h>
+#include <errno.h>
+
+#include <unistd.h>
+
+#include "buffer.h"
+#include "hexdump.h"
+
+#define BACKPAD_SIZE    16
+#define REARPAD_SIZE    16
+
+
+static struct bufnode *bufnode_new(size_t size);
+static void buffer_insert_buf(struct buffer *b, struct bufnode *node);
+static struct bufnode *buffer_remove_buf(struct buffer *b);
+
+
+void
+buffer_init(struct buffer *b, size_t sizehint)
+{
+  b->head = b->tail = 0;
+  b->nbuf = 0;
+
+  b->sizehint = sizehint;
+}
+
+
+void
+buffer_clear(struct buffer *b)
+{
+  struct bufnode *p;
+
+  while (b->head) {
+    p = buffer_remove_buf(b);
+    free(p);
+  }
+}
+
+
+static __inline__ void
+buffer_insert_buf(struct buffer *b, struct bufnode *node)
+{
+  if (!b->tail)
+    b->head = b->tail = node;
+  else {
+    b->tail->next = node;
+    b->tail = node;
+  }
+  b->nbuf++;
+}
+
+
+static __inline__ struct bufnode *
+buffer_remove_buf(struct buffer *b)
+{
+  struct bufnode *p;
+
+  if (!b->head)
+    return 0;
+
+  p = b->head;
+  if (b->head == b->tail)
+    b->head = b->tail = 0;
+  else
+    b->head = p->next;
+  b->nbuf--;
+  return p;
+}
+
+
+static struct bufnode *
+bufnode_new(size_t size)
+{
+  struct bufnode *p;
+  p = malloc(sizeof(*p) + size + BACKPAD_SIZE + REARPAD_SIZE);
+  if (!p) {
+    errno = ENOMEM;           /* redundant, according to UNIX 98 */
+    return 0;
+  }
+
+  p->size = size + BACKPAD_SIZE;
+  p->begin = p->end = p->last = p->data + BACKPAD_SIZE;
+  p->next = 0;
+  return p;
+}
+
+
+struct bufnode *
+buffer_grow_capacity(struct buffer *b, size_t size)
+{
+  struct bufnode *nptr = b->tail;
+  size_t realsize;
+  size_t remains;
+
+  realsize = b->sizehint > size ? b->sizehint : size;
+
+  if (!nptr) {
+    nptr = bufnode_new(realsize);
+    if (!nptr)
+      return 0;
+    buffer_insert_buf(b, nptr);
+  }
+  else {
+    remains = BUFNODE_AVAIL(nptr);
+
+    if (remains < size) {
+      nptr = bufnode_new(realsize);
+      if (!nptr)
+        return 0;
+      buffer_insert_buf(b, nptr);
+    }
+  }
+  assert(BUFNODE_AVAIL(nptr) >= size);
+  return nptr;
+}
+
+
+ssize_t
+buffer_fill_fd(struct buffer *b, int fd, size_t size)
+{
+  /* Design consideration
+   *
+   * Which is better, to fill the last bufnode in B from FD, or
+   * to create new bufnode and fill it from FD?
+   *
+   * Currently, I'll choose the last one. */
+  struct bufnode *nptr;
+  ssize_t readch;
+  size_t remains;
+  size_t total = 0;
+
+  while (1) {
+    if ((nptr = buffer_grow_capacity(b, 1)) == 0)
+      return -1;
+
+    /* Because of BACKPAD_SIZE, merely using NPTR->SIZE for REMAINS
+     * is not good. */
+    remains = BUFNODE_AVAIL(nptr);
+    if (remains > size)
+      remains = size;
+
+    /* TODO: EPIPE handling?? */
+
+    readch = read(fd, nptr->end, remains);
+    if (readch == -1) {
+      if (errno == EINTR || errno == EAGAIN)
+        return total;
+      else
+        return -1;
+    }
+    else if (readch == 0) {            /* EOF */
+      errno = 0;                       /* Is this necessary? */
+      return total;
+    }
+
+    nptr->end += readch;
+    total += readch;
+
+    if (total >= size)
+      break;
+  }
+  return total;
+}
+
+
+int
+buffer_printf(struct buffer *b, const char *format, ...)
+{
+  struct bufnode *nptr;
+  va_list ap;
+  int len;
+
+  va_start(ap, format);
+  len = vsnprintf(NULL, 0, format, ap);
+  va_end(ap);
+
+  nptr = buffer_grow_capacity(b, len + 1);
+  if (!nptr)
+    return -1;
+
+  va_start(ap, format);
+  vsnprintf(nptr->end, len + 1, format, ap);
+  va_end(ap);
+
+  nptr->end += len;
+
+  return len;
+}
+
+
+char *
+buffer_find(struct buffer *buf, const void *seed, size_t size,
+            struct bufnode **dst)
+{
+  struct bufnode *p;
+  char *c;
+
+  assert(size < REARPAD_SIZE);
+
+  for (p = buf->head; p != NULL; p = p->next) {
+    struct bufnode *q = p->next;
+
+    if (q)
+      memcpy(p->end, q->begin, size);
+
+    c = memmem(p->begin, p->end - p->begin + size, seed, size);
+    if (c) {
+      *dst = p;
+      return c;
+    }
+  }
+  return 0;
+}
+
+
+void
+buffer_advance(struct buffer *b, struct bufnode *n, char *next, int offset)
+{
+  struct bufnode *p;
+
+  if (!n)
+    n = b->head;
+
+  while (b->head && b->head != n) {
+    p = buffer_remove_buf(b);
+    free(p);
+  }
+
+  if (next) {
+    assert(n->data <= next && next <= n->data + n->size);
+
+    n->begin = next + offset;
+    n->last = next + offset;
+  }
+  else {
+    n->begin = n->end;
+    n->last = n->end;
+  }
+
+  if (n->begin >= n->end) {     /* N is empty. */
+    offset -= n->end - next;
+    buffer_remove_buf(b);       /* delete N from the buffer, B */
+    free(n);
+
+    n = b->head;
+    if (n) {
+      n->begin += offset;
+      n->last = n->begin;
+
+      assert(n->begin < n->end);
+    }
+    else {
+      assert(offset == 0);
+    }
+  }
+}
+
+
+ssize_t
+buffer_flush(struct buffer *b, struct bufnode *n, char *next, int fd)
+{
+  size_t remains;
+  ssize_t written;
+  ssize_t total = 0;
+
+  struct bufnode *p = b->head;
+
+  if (!n)
+    n = b->tail;
+
+  while ((p = b->head) != n && p != NULL) {
+    remains = p->end - p->begin;
+    if (remains > 0) {
+      written = write(fd, p->begin, remains);
+      if (written == -1) {
+        if (errno == EAGAIN || errno == EINTR)
+          return total;
+        return -1;
+      }
+      else {
+        total += written;
+      }
+    }
+    buffer_remove_buf(b);
+    free(p);
+  }
+
+  if (!p)
+    return total;
+
+  if (next && b->head == n)
+    remains = next - n->begin;
+  else
+    remains = n->end - n->begin;
+
+  written = write(fd, n->begin, remains);
+  if (written == -1) {
+    if (errno == EAGAIN || errno == EINTR)
+      return 0;
+    return -1;
+  }
+  else {
+    // n->begin += written;
+    buffer_advance(b, b->head, n->begin + written, 0);
+    total += written;
+  }
+  return total;
+}
+
+
+// void
+// buffer_advance(struct buffer *b, struct bufnode *n, char *next, int offset)
+
+
+void
+buffer_dump(FILE *fp, struct buffer *b)
+{
+  struct bufnode *p;
+
+  fprintf(fp, "head: %p\n", b->head);
+  fprintf(fp, "tail: %p\n", b->tail);
+  fprintf(fp, "nbuf: %zd\n", b->nbuf);
+  fprintf(fp, "size: %zd\n", b->sizehint);
+  fprintf(fp, "back: %d\n", BACKPAD_SIZE);
+  fprintf(fp, "rear: %d\n", REARPAD_SIZE);
+
+  for (p = b->head; p != NULL; p = p->next) {
+    fprintf(fp, "bufnode[%p]:\n", p);
+    fprintf(fp, "   next: %p\n", p->next);
+    fprintf(fp, "  begin: %p\n", p->begin);
+    fprintf(fp, "   last: %p\n", p->last);
+    fprintf(fp, "    end: %p\n", p->end);
+    fprintf(fp, "   size: %zd\n", p->size);
+    hexdump(fp, 1, -1, p->data, p->data + p->size + BACKPAD_SIZE);
+  }
+}
+
+
+#if 0
+void
+buf_advance(struct buf *buf, char *spot)
+{
+  assert(spot >= buf->begin);
+  assert(spot <= buf->end);     /* TODO: (spot < buf->end)? */
+
+  if (spot == buf->begin)
+    return;
+
+  if (spot == buf->end) {       /* buf empty */
+    buf->begin = buf->last = buf->end = buf->data;
+  }
+  else if (spot >= buf->data + (buf->size >> 2)) {
+    size_t remains = buf->end - spot;
+    memmove(buf->data, spot, remains);
+    buf->data = buf->begin = buf->last;
+    buf->end = buf->data + remains;
+  }
+  else {
+    buf->begin = spot;
+    if (buf->last < spot)
+      buf->last = buf->begin;
+  }
+}
+
+
+/*
+ * Flush the buf contents from BUF->BEGIN to HERE.
+ */
+int
+buf_flush_fd(struct buf *buf, int fd, char *here)
+{
+  size_t remains;
+  ssize_t written;
+
+  if (!here)
+    here = buf->end;
+  remains = here - buf->begin;
+
+  written = write(fd, buf->begin, remains);
+  if (written == -1) {
+    if (errno == EAGAIN || errno == EINTR)
+      return 0;
+    else
+      return -1;
+  }
+  else {
+    buf_advance(buf, buf->begin + written);
+    return written;
+  }
+}
+
+
+/*
+ *
+ * Fill BUF by reading from FD.
+ *
+ * returns:
+ *
+ *   buf full (-1, errno = ENOMEM)
+ *   read error (-1, errno = ERROR)
+ *   EINTR (0, errno = EINTR)
+ *   EOF  (0, errno = 0)
+ *   read N byte(s) (read-bytes, errno = ??)
+ */
+int
+buf_fill_fd(struct buf *buf, int fd)
+{
+  size_t remains = buf->data + buf->size - buf->end;
+  ssize_t readch;
+
+  /* TODO: EPIPE handling?? */
+
+  buf->last = buf->end;
+
+  if (remains == 0) {           /* BUF is full */
+    errno = ENOMEM;
+    return -1;                  /* return what?? */
+  }
+
+  readch = read(fd, buf->end, remains);
+  if (readch == -1) {
+    if (errno == EINTR || errno == EAGAIN)
+      return 0;                 /* return what? */
+    else
+      return -1;                /* return what? error */
+  }
+  else if (readch == 0) {            /* EOF */
+    errno = 0;
+    return 0;                        /* return what? */
+  }
+
+  buf->end += readch;
+  return readch;
+}
+
+
+static int
+fdprintf(int fd, const char *format, ...)
+{
+  va_list ap;
+  int len;
+  char *buf;
+
+  va_start(ap, format);
+  len = vsnprintf(0, 0, format, ap);
+  va_end(ap);
+
+  buf = malloc(len + 1);
+  va_start(ap, format);
+  len = vsnprintf(buf, len + 1, format, ap);
+  va_end(ap);
+
+  write(fd, buf, len);
+  free(buf);
+  return len;
+}
+
+
+read_cb()
+{
+  rbuf rb;
+
+  rb.fill();
+
+  offset = rb.parse_request();
+  if (offset == -1) {           /* not fully received for complete request */
+    return;
+  }
+
+  if (can_handle_request()) { /* GET, HEAD, OPTIONS or DELETE */
+    rb.advance(offset);
+    call_request_handler();
+  }
+  else {                      /* POST or PUT */
+    call_post_or_put_handler();
+  }
+}
+#endif
+
+
+#ifdef TEST_BUFFER
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int
+set_nonblock(int fd)
+{
+  int flags;
+
+  flags = fcntl(fd, F_GETFL);
+  if (flags == -1) {
+    fprintf(stderr, "fcntl failed: %s\n", strerror(errno));
+    return -1;
+  }
+  if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+    fprintf(stderr, "fcntl failed: %s\n", strerror(errno));
+    return -1;
+  }
+  return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+  struct buffer b;
+
+  // socket
+#if 0
+  fd = open(argv[1], O_RDONLY | O_NONBLOCK);
+  if (fd == -1) {
+    fprintf(stderr, "open failed: %s\n", strerror(errno));
+    return 1;
+  }
+#endif  /* 0 */
+  set_nonblock(0);
+  set_nonblock(1);
+
+  setvbuf(stdout, NULL, _IONBF, 0);
+
+  buffer_init(&b, 64);
+  buffer_fill_fd(&b, 0, -1);
+
+  buffer_dump(stderr, &b);
+
+  printf("========================\n");
+
+  {
+    struct bufnode *dst;
+    char *p = buffer_find(&b, "HD_OFFSET", 9, &dst);
+
+    printf("buffer_find(): returns %p, dst[%p]\n", p, dst);
+    if (p)
+      buffer_advance(&b, dst, p, 9);
+  }
+
+  buffer_flush(&b, NULL, NULL, STDOUT_FILENO);
+
+  // buffer_clear(&b);
+  printf("========================\n");
+
+  buffer_dump(stderr, &b);
+  return 0;
+}
+#endif
---
+++ buffer.h
@@ -0,0 +1,156 @@
+#ifndef BUFFER_H__
+#define BUFFER_H__
+
+#include <string.h>
+
+struct bufnode {
+  char *begin, *end;
+  char *last;
+  size_t size;
+  struct bufnode *next;
+  char data[0];
+};
+
+struct buffer {
+  struct bufnode *head;
+  struct bufnode *tail;
+
+  size_t nbuf;
+
+  size_t sizehint;
+};
+
+#define BUFNODE_AVAIL(n)        ((n)->data + (n)->size - (n)->end)
+
+void buffer_init(struct buffer *b, size_t sizehint);
+
+/*
+ * Append data from FD with SIZE byte(s) in the buffer, B.
+ *
+ * It returns the number of actual read byte(s).  On EOF, it returns
+ * zero with 'errno' cleared.  It is possible that this function read
+ * less than SIZE byte(s).
+ *
+ * When FD is marked non-blocking and there is nothing to read
+ * currently, it returns zero with 'errno' set to EAGAIN.  If it is
+ * signaled, it returns zero with 'errno' set to EINTR.  Otherwise (on
+ * error) it returns -1.
+ */
+ssize_t buffer_fill_fd(struct buffer *b, int fd, size_t size);
+
+
+/*
+ * Find SEED with SIZE byte(s) from the buffer contents.
+ *
+ * It returns the pointer to SEED, and DST is set to the target bufnode.
+ * It returns NULL when SEED is not found.
+ */
+char *buffer_find(struct buffer *buf, const void *seed, size_t size,
+                  struct bufnode **dst);
+
+/*
+ * Advance the buffer position so that the beginning of the buffer
+ * is set to NEXT of N.  All previous bufnode before N will be released.
+ *
+ * If offset is nonzero, the buffer position will advance in addition
+ * of OFFSET bytes.  In other words, the beginning of the contents is
+ * set to NEXT + OFFSET.  This is especially useful when you want to
+ * advance the buffer from the returned pointer of buffer_find() with
+ * the size of the seed.   For example:
+ *
+ * char *p = buffer_find(buffer, seed, seed_size, &nextnode);
+ * ...
+ * buffer_advance(buffer, nextnode, p, seed_size);
+ */
+void buffer_advance(struct buffer *b, struct bufnode *n,
+                    char *next, int offset);
+
+/*
+ * Flush (write) the buffer contents into the file FD.
+ *
+ * It will write the contents from the beginning of the buffer to the
+ * point NEXT in the bufnode N.  If N is NULL, the first BUFNODE in
+ * the B is used as a default.  If NEXT is NULL, it is assumes that
+ * the end of the N.  To write the whole contents of B into FD, you
+ * may pass NULL for both of N and NEXT.
+ *
+ * It may call write(2) multiple times if there are more than one
+ * bufnode in B.
+ *
+ * It returns the total number of byte(s) it wrote on success.  If FD
+ * is marked as non-blocking, this function may return without writing
+ * fully.  In this case, this function returns the number of byte(s)
+ * it wrote so far, and 'errno' will be set to EAGAIN.  (Likewise, if
+ * it signaled, it returns the number of byte(s) it wrote, then
+ * 'errno' will be set to EINTR.)
+ *
+ * On error (except neither EAGAIN nor EINTR), this function returns
+ * -1, and 'errno' is set appropriately by write(2).  Note that if
+ * there occurs an error after succesfully wrote some contents, there
+ * is no way to get the number of byte(s) it wrote so far.
+ */
+ssize_t buffer_flush(struct buffer *b, struct bufnode *n, char *next, int fd);
+
+/*
+ * Convenient function to fill the buffer with printf(3) like formatted
+ * string.
+ *
+ * Note that this function does not append '\0'.
+ *
+ * It returns the number of bytes that it append on success.
+ * Otherwise, it returns -1.
+ */
+int buffer_printf(struct buffer *b, const char *format, ...)
+  __attribute__((format (printf, 2, 3)));
+
+
+/* Return the number of availabe byte(s) that a buffer can hold without
+ * allocating additional bufnode. */
+#define buffer_roomsize(b)      ((b)->tail ? BUFNODE_AVAIL((b)->tail) : 0)
+
+/*
+ * Add more bufnode to the buffer B if the current bufnodes can't hold
+ * additional SIZE byte(s).
+ */
+struct bufnode *buffer_grow_capacity(struct buffer *b, size_t size);
+
+
+#define buffer_1add_fast(b, ch)        (*(b)->tail->end++ = ch)
+
+static __inline__ int
+buffer_1add(struct buffer *b, char ch)
+{
+  struct bufnode *nptr = buffer_grow_capacity(b, 1);
+  if (!nptr)
+    return 0;
+  *nptr->end++ = ch;
+  return 1;
+}
+
+
+static __inline__ ssize_t
+buffer_fill(struct buffer *b, const void *data, size_t size)
+{
+  struct bufnode *nptr = buffer_grow_capacity(b, size);
+
+  if (!nptr)
+    return -1;
+
+  nptr->last = nptr->end;
+  memcpy(nptr->end, data, size);
+  nptr->end += size;
+
+  return size;
+}
+
+
+#if 0
+/* TODO */
+ssize_t buffer_flush(struct buffer *b, int fd);
+buffer_getc();
+
+char *buffer_gets(); // next buffer operation may invalidate the returned ptr
+
+#endif  /* 0 */
+
+#endif /* BUFFER_H__ */
